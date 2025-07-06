cb8ce5aa4f93321d24c11985d18ea145f3cb183a
ShellyDiber <shellydiber2@gmail.com>
2025-07-06 12:20:59 +0300

    css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,26 @@ import Main from './pages/MainPage';
 import AdminSignup from './pages/AdminSignup';
 import Results from './pages/ResultsAdmin';
 import LivePage from './pages/Live';
+import { useSong } from './context/songContext';
 
 
 function App() {
+
+const { user} = useSong();
+console.log('Current user:', user);
+
+if (!user) {
+    // If user is not logged in, redirect to login page
+    return (
+      <Routes>
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/adminSignup" element={<AdminSignup />} />
+    <Route path="*" element={<Login />} />
+    
+    </Routes>
+    )
+  }else{
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
@@ -18,8 +35,9 @@ function App() {
       <Route path="/adminSignup" element={<AdminSignup />} />
       <Route path="/results" element={<Results />} /> 
       <Route path="/live" element={<LivePage />} /> 
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
-
+}
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import App from './App';
 import './index.css';
 import { SongProvider } from './context/songContext';
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
